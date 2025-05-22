8b2c304b138e33577f4661726fb2c513b4534653
Amritha raj herle <herleraja@gmail.com>
2025-05-22 17:46:45 +0200

    Updates

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/preset-create-react-app",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+};
+export default config;
---
+++ .storybook/manager.js
@@ -0,0 +1,34 @@
+import { addons } from "@storybook/manager-api";
+import theme from "./theme";
+
+addons.register('togglePanel', (api) => {
+  api.togglePanel(false);
+});
+addons.register('toggleFullscreen', (api) => {
+  api.toggleFullscreen(true);
+});
+
+addons.setConfig({
+  isFullscreen: true,
+  showNav: false,
+  showPanel: false,
+  panelPosition: "bottom",
+  enableShortcuts: false,
+  showToolbar: false,
+  theme,
+  selectedPanel: undefined,
+  initialActive: "canvas",
+  sidebar: {
+    showRoots: false,
+    collapsedRoots: ["other"],
+    disable: true,
+  },
+  docs: { disable: true },
+  toolbar: {
+    title: { hidden: true },
+    zoom: { hidden: true },
+    eject: { hidden: true },
+    copy: { hidden: true },
+    fullscreen: { hidden: true },
+  },
+});
